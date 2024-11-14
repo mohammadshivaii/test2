1c57bbdacaa2ae1acc1bdef418cc6447457f19c3
mohammadshivaii <mohammadshivaii4@gmail.com>
2024-11-14 19:20:56 +0330

    test

--- test.py
+++ test.py
@@ -1,1 +1,72 @@
-print("hello world ")
+# print("hello world "
+# def shayan (a,b)
+# # class
+# # cunstructor ------   > مقدار دهی و معین ردن ورودی هاست
+# class Test :
+#     def __init__(self,a,b):
+#         self.a=a
+#         self.b=b
+#     def jam(self):
+#         return self.a + self.b
+#     def menha(self):
+#         return  self.a - self.b
+#
+# obj1=Test(10,5)
+# obj2=Test(15,6)
+# # print(obj1.jam())
+# # print(obj1.menha())
+# # print(obj2.jam())
+#
+#
+# class Book:
+#     def __init__(self,author,title):
+#         self.author=author
+#         self.title=title
+#
+#     def info(self):
+#         print(self.title + ":" + self.author)
+#
+# oj1=Book("hashemi","python")
+# oj2=Book("habibi","c++")
+# oj3=Book("hamidi","rust")
+# oj1.info()
+# oj2.info()
+# oj3.info()
+# class Test:
+#     def __init__(self,a,b):
+#         self.a=a    # public
+#         self.__b=b   # privite
+#
+#     def f(self):
+#         self.__b+=1
+#         print(self.__b)
+#
+# oj=(Test(1,5))
+# oj.f()
+# class S:
+#     def __init__(self,a):
+#         self.__a=a # private
+#
+#     def f(self):
+#         print(self.__a,end=" ")
+#         self.__a+=1
+#         return (self.__a)
+#     def g (self,m):
+#         print(m + self.f())
+#
+# ob=S(1)
+# print(ob.f())
+# ob.g(5)
+
+# s="amin"
+# print(type(s))
+# x=1
+# y=2
+# x,y=y-1,x+2
+# print(x)
+
+x=" shayanhashemi"
+print(x[0])
+print(x[5:8])
+print(x[:5])
+print(x[5:])
